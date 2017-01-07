649b4569874882f85018a3e9cdfa0821d7968c64
speschellk <kaychristensen@gmail.com>
2017-01-06 22:27:28 -0800

    Add YouTube API GET request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {'key': options.key, 'q': options.query, 'part': 'snippet', 'maxResults': options.max, 'type': 'video', 'videoEmbeddable': 'true'},
+    dataType: 'json',
+    success: (data) => {
+      console.log('you are so good');
+      callback(data.items); // do something with this
+    } 
+  });
 };
 
 window.searchYouTube = searchYouTube;
